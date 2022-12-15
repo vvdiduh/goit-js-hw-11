09ea50b69335d14a1056faef80632005c38ea313
vvdiduh <vvdiduh@gmail.com>
2022-12-16 00:24:27 +0200

    finisf task, no stile

--- src/js/index.js
+++ src/js/index.js
@@ -2,53 +2,60 @@ import PhotosApiService from './photos'
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
-    loadMoreBtn: document.querySelector('.load-more')
+    loadMoreBtn: document.querySelector('.load-more'),
+    gallery: document.querySelector('.gallery')
 }
-
 const photosApiService = new PhotosApiService();
+
+refs.loadMoreBtn.style.opacity = '0';
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', loadMore)
 
-// fetchPhoto('cat')
-
-// function fetchPhoto(value) {
-//     return
-// }
-
 function loadMore() {
-    photosApiService.fetchArticles();
+    photosApiService.fetchArticles()
+        .then(photos => {
+            refs.gallery.insertAdjacentHTML('beforeend', markupList(photos)) 
+        });
 }
 
 function onSearch(e) {
     e.preventDefault();
-
+    clearGallery();
     photosApiService.query = e.currentTarget.searchQuery.value;
-    console.log();
-
-    photosApiService.fetchArticles();
+    if (photosApiService.query === '') {
+        return
+    }
+    photosApiService.resetPage();
+    photosApiService.fetchArticles()
+        .then(photos => {
+            refs.gallery.insertAdjacentHTML('beforeend', markupList(photos))
+        });
+    refs.loadMoreBtn.style.opacity = '1';
 }
 
-// function markupList(photos) {
-//     return photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-//         return `
-//         <div class="photo-card">
-//             <img src="" alt="" loading="lazy"/> 
-//             <div class="info">
-//                 <p class="info-item">
-//                     <b>Likes</b>
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Views</b>
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Comments</b>
-//                 </p>
-//                 <p class="info-item">
-//                     <b>Downloads</b>
-//                 </p>
-//             </div>
-//         </div>`
-//     })
-    
-// }
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+}
+
+function markupList(photos) {
+    return photos.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+        <div class="photo-card">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy"/> 
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes ${likes}</b>
+                </p>
+                <p class="info-item">
+                    <b>Views ${views}</b>
+                </p>
+                <p class="info-item">
+                    <b>Comments ${comments}</b>
+                </p>
+                <p class="info-item">
+                    <b>Downloads ${downloads}</b>
+                </p>
+            </div>
+        </div>`});
+}
--- src/js/photos.js
+++ src/js/photos.js
@@ -1,16 +1,25 @@
 export default class PhotosApiService {
     constructor() {
         this.searchQuery = '';
+        this.page = 1;
     }
     
     fetchArticles() {
-        fetch(`https://pixabay.com/api/?key=32083000-0930ed6e251f9a7620e985678&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
+        return fetch(`https://pixabay.com/api/?key=32083000-0930ed6e251f9a7620e985678&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
         .then((response) => {
             if (!response.ok) {
                 throw new Error(response.status);
             }
             return response.json();
+        })
+        .then(photos => {
+            this.page += 1;
+            return photos.hits;
         });
+    }
+
+    resetPage() {
+        this.page = 1;
     }
 
     get query() {
