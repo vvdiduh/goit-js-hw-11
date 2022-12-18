d6f0e1819327adfffdb4596aad611728272915c6
vvdiduh <vvdiduh@gmail.com>
2022-12-18 20:12:29 +0200

    finish!!!!!!!!

--- src/js/photos.js
+++ src/js/photos.js
@@ -7,16 +7,18 @@ export default class PhotosApiService {
     }
     
     fetchArticles() {
-        return fetch(`https://pixabay.com/api/?key=32083000-0930ed6e251f9a7620e985678&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+        return axios.get(`https://pixabay.com/api/?key=32083000-0930ed6e251f9a7620e985678&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
         .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
+            // if (!response.ok) {
+            //     throw new Error(response.status);
+            // }
+            // console.log(response.data.hits)
+            return response
         })
         .then(photos => {
             this.page += 1;
-            return photos.hits;
+            console.log(photos.hits)
+            return photos.data.hits;
         });
     }
 
